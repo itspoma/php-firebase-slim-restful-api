7c6e0c5de8617e45eaa6fbfc910e107036a399dc
itspoma <itspoma@gmail.com>
2016-07-27 09:54:40 +0200

    remove credentials from repo

--- src/config/development.sample.php
+++ src/config/development.sample.php
@@ -2,5 +2,5 @@
 $config['debug'] = true;
 $config['mode'] = 'development';
 
-$config['firebase.url'] = 'https://php-firebase-slim-restfu-d0133.firebaseio.com/';
-$config['firebase.secret'] = 'YWEoXhUGyP66iDouHqh8Wo8YtyYStpWghJwmhNyh';
+$config['firebase.url'] = 'https://(...).firebaseio.com/';
+$config['firebase.secret'] = '...';
